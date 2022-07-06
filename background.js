b884da9b8f47ceadbcb53b221d89c4a4534802a2
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-07-06 23:27:59 +0200

    feat: allows to enable iframes for each tab individually

--- background.js
+++ background.js
@@ -1,25 +1,51 @@
 /* global browser */
 
-let active = true;
+let excludeTabIdList = new Set();
 
 const filter = { urls: ["<all_urls>"] };
 const extraInfoSpec = ["blocking"];
 
-browser.browserAction.setBadgeText({text: (active?'on':'off') });
-browser.browserAction.setBadgeBackgroundColor({color: 'green'});
-
-function BAonClicked() {
-    active = !active; // toggle active state
-	browser.browserAction.setBadgeText({text: (active?'on':'off') });
-    browser.browserAction.setBadgeBackgroundColor({color: (active?'green':'red')});
+function updateBadge(tabId, badgeText, badgeBackgroundColor,badgeTitle){
+        browser.browserAction.setBadgeText({tabId, text: badgeText});
+        browser.browserAction.setBadgeBackgroundColor({tabId, color: badgeBackgroundColor});
+        browser.browserAction.setTitle({tabId, title: badgeTitle});
 }
 
-function onBeforeRequest (details) {
-    if( active && details.parentFrameId !== -1){
+
+function onBeforeWebRequest (details) {
+    if( !excludeTabIdList.has(details.tabId) && details.parentFrameId !== -1){
         return {cancel: true};
     }
 }
 
-browser.webRequest.onBeforeRequest.addListener(onBeforeRequest,filter,extraInfoSpec);
-browser.browserAction.onClicked.addListener(BAonClicked);
+function onTabRemoved(tabId){
+    if(excludeTabIdList.has(tabId)){
+        excludeTabIdList.delete(tabId);
+    }
+}
+
+function onTabUpdated(tabId){
+    if(excludeTabIdList.has(tabId)){
+        updateBadge(tabId, 'Off', 'red', "Not Blocking iframes. Click to Enable for this Tab!");
+    }else{
+        updateBadge(tabId, 'On', 'green', "Blocking iframes. Click to Enable for this Tab!");
+    }
+}
+
+function onBrowserActionClicked(tab) {
+    const tabId = tab.id;
+    if(excludeTabIdList.has(tabId)){
+        excludeTabIdList.delete(tabId);
+    }else{
+        excludeTabIdList.add(tab.id);
+    }
+    onTabUpdated(tabId);
+}
+
+updateBadge(undefined, 'On', 'green', "Blocking iframes. Click to Enable for this Tab!");
+
+browser.webRequest.onBeforeRequest.addListener(onBeforeWebRequest,filter,extraInfoSpec);
+browser.browserAction.onClicked.addListener(onBrowserActionClicked);
+browser.tabs.onRemoved.addListener(onTabRemoved);
+browser.tabs.onUpdated.addListener(onTabUpdated, {properties: ["status"]});
 
